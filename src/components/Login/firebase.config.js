4a058087159f53bebffc37570e5d3418689c1197
ratnakarmaker <73543192+ratnakarmaker@users.noreply.github.com>
2021-04-01 20:14:22 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAAfmpaQwcnhiJS1BPeYVALaEYlGFgl8s",
+  authDomain: "e-stationarybd.firebaseapp.com",
+  projectId: "e-stationarybd",
+  storageBucket: "e-stationarybd.appspot.com",
+  messagingSenderId: "46338625043",
+  appId: "1:46338625043:web:7a7c03bb8e311d2fd2f9e6"
+};
+
+  export default firebaseConfig;
